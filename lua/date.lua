862689b757d7142cd6274f8049b7814b1175df9c
lavender010101 <lavender010101@163.com>
2023-04-10 12:21:44 +0800

    Change config

--- lua/date.lua
+++ lua/date.lua
@@ -1,39 +1,155 @@
---[[
-date_translator: 将 `date` 翻译为当前日期
-
-translator 的功能是将分好段的输入串翻译为一系列候选项。
-
-欲定义的 translator 包含三个输入参数：
- - input: 待翻译的字符串
- - seg: 包含 `start` 和 `_end` 两个属性，分别表示当前串在输入框中的起始和结束位置
- - env: 可选参数，表示 translator 所处的环境（本例没有体现）
-
-translator 的输出是若干候选项。
-与通常的函数使用 `return` 返回不同，translator 要求您使用 `yield` 产生候选项。
-
-`yield` 每次只能产生一个候选项。有多个候选时，可以多次使用 `yield` 。
-
-请看如下示例：
---]]
-
+ --lua语言中的注释用“--”
 local function translator(input, seg)
-   -- 如果输入串为 `date` 则翻译
    if (input == "date") then
-      --[[ 用 `yield` 产生一个候选项
-           候选项的构造函数是 `Candidate`，它有五个参数：
-            - type: 字符串，表示候选项的类型
-            - start: 候选项对应的输入串的起始位置
-            - _end:  候选项对应的输入串的结束位置
-            - text:  候选项的文本
-            - comment: 候选项的注释
-       --]]
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
-      --[[ 用 `yield` 再产生一个候选项
-           最终的效果是输入法候选框中出现两个格式不同的当前日期的候选项。
-      --]]
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
+------------------------------------------------------------------------------------
+--普通日期1，类似2022年01月02日
+date1=os.date("%Y年%m月%d日")
+date_y=os.date("%Y") --取年
+date_m=os.date("%m") --取月
+date_d=os.date("%d") --取日
+--yield(Candidate("date", seg.start, seg._end, date1, " "))
+------------------------------------------------------------------------------------
+--普通日期2，类似2022年1月1日
+num_m=os.date("%m")+0
+num_m1=math.modf(num_m)
+num_d=os.date("%d")+0
+num_d1=math.modf(num_d)
+date2=os.date("%Y年")..tostring(num_m1).."月"..tostring(num_d1).."日"
+yield(Candidate("date", seg.start, seg._end, date2, " "))
+------------------------------------------------------------------------------------
+--普通日期3，类似1月1日
+num_m=os.date("%m")+0
+num_m1=math.modf(num_m)
+num_d=os.date("%d")+0
+num_d1=math.modf(num_d)
+date3=tostring(num_m1).."月"..tostring(num_d1).."日"
+yield(Candidate("date", seg.start, seg._end, date3, " "))
+yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), " "))
+yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), " "))
+------------------------------------------------------------------------------------
+--大写日期，类似二〇二〇年十一月二十六日
+date_y=date_y:gsub("%d",{
+["1"]="一",
+["2"]="二",
+["3"]="三",
+["4"]="四",
+["5"]="五",
+["6"]="六",
+["7"]="七",
+["8"]="八",
+["9"]="九",
+["0"]="〇",
+})
+date_y=date_y.."年"
+date_m=date_m:gsub("%d",{
+["1"]="一",
+["2"]="二",
+["3"]="三",
+["4"]="四",
+["5"]="五",
+["6"]="六",
+["7"]="七",
+["8"]="八",
+["9"]="九",
+["0"]="",
+})
+date_m=date_m.."月"
+if num_m1==10 then date_m="十月" end
+if num_m1==11 then date_m="十一月" end
+if num_m1==12 then date_m="十二月" end
+date_d=date_d:gsub("%d",{
+["1"]="一",
+["2"]="二",
+["3"]="三",
+["4"]="四",
+["5"]="五",
+["6"]="六",
+["7"]="七",
+["8"]="八",
+["9"]="九",
+["0"]="",
+})
+date_d=date_d.."日"
+if num_d1>9 then
+if num_d1<19 then
+date_d="十"..string.sub(date_d,4,#date_d)
+end
+end
+if num_d1>19 then date_d=string.sub(date_d,1,3).."十"..string.sub(date_d,4,#date_d) end
+date4=date_y..date_m..date_d
+yield(Candidate("date", seg.start, seg._end, date4, " "))
+------------------------------------------------------------------------------------
+--英文日期
+    local date_d=os.date("%d")
+    local date_m=os.date("%m")
+    local date_y=os.date("%Y")
+    local date_m1=""
+    local date_m2=""
+    if date_m=="01" then 
+       date_m1="Jan."
+       date_m2="January"
+    end
+    if date_m=="02" then 
+       date_m1="Feb."
+       date_m2="February"
+    end
+    if date_m=="03" then 
+       date_m1="Mar."
+       date_m2="March"
+    end
+    if date_m=="04" then 
+       date_m1="Apr."
+       date_m2="April"
+    end
+    if date_m=="05" then 
+       date_m1="May."
+       date_m2="May"
+    end
+    if date_m=="06" then 
+       date_m1="Jun."
+       date_m2="June"
+    end
+    if date_m=="07" then 
+       date_m1="Jul."
+       date_m2="July"
+    end
+    if date_m=="08" then 
+       date_m1="Aug."
+       date_m2="August"
+    end
+    if date_m=="09" then 
+       date_m1="Sept."
+       date_m2="September"
+    end
+    if date_m=="10" then 
+       date_m1="Oct."
+       date_m2="October"
+    end
+    if date_m=="11" then 
+       date_m1="Nov."
+       date_m2="November"
+    end
+    if date_m=="12" then 
+       date_m1="Dec."
+       date_m2="December"
+    end
+
+     if date_d=="0" then 
+       symbal="st" 
+     elseif date_d=="1" then
+       symbal="nd" 
+     elseif date_d=="2" then 
+       symbal="rd" 
+     else
+       symbal="th"
+     end
+date5=date_m1.." "..date_d..symbal..", "..date_y
+date6=date_m2.." "..date_d..symbal..", "..date_y
+
+yield(Candidate("date", seg.start, seg._end, date5, " "))
+yield(Candidate("date", seg.start, seg._end, date6, " "))
+yield(Candidate("date", seg.start, seg._end, os.date("%Y%m%d"), " "))
    end
 end
-
--- 将上述定义导出
+------------------------------------------------------------------------------------
 return translator
--- lua/time.lua
+++ lua/time.lua
@@ -1,11 +1,10 @@
---[[
-time_translator: 将 `time` 翻译为当前时间
---]]
-
+  --lua语言中的注释用“--”
 local function translator(input, seg)
-   if (input == "time") then
-      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
+   if (input == "time") then         --关键字更改，你也可以用or语句定义多个关键字
+      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), " "))
+      yield(Candidate("time", seg.start, seg._end, os.date("%H点%M分"), " "))
+      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), " "))
+      yield(Candidate("time", seg.start, seg._end, os.date("%H点%M分%S秒"), " "))
    end
 end
-
 return translator
